535b0847730cf1fff547c10c4164eb82a65c6f4e
Haris Sucipto <harisssucipto@gmail.com>
2016-12-20 22:05:26 +0700

    tambahkan database dan fungsi bikin calculator

--- app.js
+++ app.js
@@ -30,7 +30,22 @@ var calculatorSchema = new mongoose.Schema({
 
 var Calculator = mongoose.model("Calculator", calculatorSchema);
 
+Calculator.create({
+        name: "binomial",
+        image: "images/binomial.png",
+        url: "calculators/binomial",
+        description: "Binomial adalah jumlah keberhasilan dalam n percobaan ya/tidak dimana setiap percobaan memiliki probalitas"
+    },
+    function(err, calculator) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Kalkulator " + calculator.name + "Berhasil dibikin");
+            console.log(calculator);
+        }
+    }
 
+);
 
 
 
