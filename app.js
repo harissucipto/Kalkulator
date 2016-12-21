e354dc3c8ad27faa0f4ba12615bf7e5b3312a275
Haris Sucipto <harisssucipto@gmail.com>
2016-12-21 17:56:35 +0700

    add handle signup logic

--- app.js
+++ app.js
@@ -40,9 +40,25 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// show register form
 app.get("/register", function(req, res) {
     res.render("register");
 });
+
+// handle signup logic
+app.post("/register", function(req, res) {
+    var newUser = new User({ username: req.body.username, password: req.body.password });
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/calculators");
+        });
+    });
+});
+
 
 
 // =======================
